5111021841a7fe45e6413a357e1e47dc1d74c0c4
Pranav Kumar <Gyannik7772@gmail.com>
2026-04-18 22:08:41 +0530

    to-do add/delete

--- app-2/src/app/app.ts
+++ app-2/src/app/app.ts
@@ -3,10 +3,38 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('app-2');
+  
+  tasks = signal([{
+    id:1,
+    title: "study",
+    isCompleted: false
+  },]);
+
+  title = signal("");
+
+  addTask(){
+    if(this.title()){
+      this.tasks.update((item)=>(
+        [...item, {id: this.tasks().length+1, title: this.title(), isCompleted:false}]
+      ));
+    }
+    
+    this.title.set("");
+  }
+  addTask1(event: String){
+    // console.log(event);
+    if(event === 'Enter'){
+      this.addTask();
+    }
+  }
+
+  deleteTasks(val:number){
+    this.tasks().splice(val, 1);
+  }
+
 }
