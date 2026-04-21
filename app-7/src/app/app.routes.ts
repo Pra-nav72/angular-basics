97da57a8c9df2f24b55680080a3c6a2aaffaaf0a
Pranav Kumar <Gyannik7772@gmail.com>
2026-04-21 19:33:52 +0530

    404 & redirecting to other page

--- app-7/src/app/app.routes.ts
+++ app-7/src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Contact } from './pages/contact/contact';
 import { About } from './pages/about/about';
+import { PageNotFound } from './pages/page-not-found/page-not-found';
 
 export const routes: Routes = [
     {path:"", component:Home},
     {path:"contact", component:Contact},
     {path:"about", component:About},
+    {path:"pranav", redirectTo:'about'},
+    {path:"**", component:PageNotFound},
 ];
